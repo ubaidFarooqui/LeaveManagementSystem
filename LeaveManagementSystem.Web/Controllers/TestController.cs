2404e3133ab5a497aceb9d2c5492baa4a8e57c14
Ubaid Farooqui <farooqui.ubaid@gmail.com>
2025-11-03 03:27:48 +0100

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Ubaid Ahmed Farooqui",
+                DateOfBirth = new DateTime(1987,12,09)
+            
+            };
+            return View(data);
+        }
+    }
+}
